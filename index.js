790c41b3832bd6b34ad1fa763d749c454f0108cf
ganeshdontharaboina26 <ganeshdontharaboina26@gmail.com>
2025-02-01 15:55:05 +0530

    -ok

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-for (let ilu = 0; ilu < 99; ilu++) {
-  console.log(ilu);  // This will print numbers 0 to 4
+// for (let ilu = 0; ilu < 99; ilu++) {
+//   console.log(ilu);  // This will print numbers 0 to 4
+// }
+let i=1;
+while    (i>6){
+  console.log(i);
+  i++
 }
