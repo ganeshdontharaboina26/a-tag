8442b2ecbcbdf4aa58e9a00b949caa84490f5e50
ganeshdontharaboina26 <ganeshdontharaboina26@gmail.com>
2025-02-01 15:47:57 +0530

    y

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-for (let i = 0; i < 5; i++) {
-  console.log(i);  // This will print numbers 0 to 4
+for (let ilu = 0; ilu < 99; ilu++) {
+  console.log(ilu);  // This will print numbers 0 to 4
 }
---
+++ python.py
@@ -0,0 +1,9 @@
+num1=3
+num2=5
+print(num1+num2)
+print(num1*num2)
+print(num1-num2)
+ramesh_age = int(input("enter ramesh age"))
+suresh_age= int (input("enter  suresh age"))
+naresh_age= int (input("enter naresh age"))
+
